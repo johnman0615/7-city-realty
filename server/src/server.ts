763e8228012baed7a7db68910b5ebb7ff70a853b
jahirusher <jahirusher2@icloud.com>
2025-03-30 20:00:52 -0400

    server.ts file

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,18 +1,22 @@
 import express from "express";
-import cors from "cors";
 import dotenv from "dotenv";
-import authRoutes from "./routes/authRoutes.ts";
-import sequelize from "./config/connection.ts";
+import cors from "cors";
+import authRoutes from "./routes/auth-routes";
+import propertyRoutes from "./routes/api/property-routes";
+import sequelize from "./config/connection";
 
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use("/api/auth", authRoutes);
+
+app.use("/auth", authRoutes);
+app.use("/api/properties", propertyRoutes);
 
 const PORT = process.env.PORT || 3001;
 
 sequelize.sync().then(() => {
   app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 });
+
