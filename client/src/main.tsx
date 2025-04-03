7b8fb2bfed1a6a75e9e3dc5eaf13eef1f4aa3792
heimsharon <heimsharon@hotmail.com>
2025-04-02 20:35:41 -0400

    Changes to be committed:

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,11 +1,9 @@
-<<<<<<< HEAD
 import React from "react";
 import ReactDOM from "react-dom/client";
+;
 import App from "./App";
 import "./styles/base.css"; 
 import "./styles/shared.css";
-import "./styles/buttons.css";
-import "./styles/navbar.css"; // Ensure navbar.css is imported last
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
@@ -13,20 +11,11 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   </React.StrictMode>
 );
 
-=======
-import ReactDOM from 'react-dom/client';
+
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import './index.css';
 
-import App from './App.tsx';
 
-import ErrorPage from './pages/ErrorPage.tsx';
-import Home from './pages/HomePage.tsx';
-import Login from './pages/Login.tsx';
-import RentPage from './pages/RentPage.tsx';
-import BuyPage from './pages/BuyPage.tsx';
-import SellPage from './pages/SellPage.tsx';
-import PropertyDetail from './pages/PropertyDetail.tsx';
+
 
 const router = createBrowserRouter([
   {
@@ -66,4 +55,3 @@ const rootElement = document.getElementById('root');
 if (rootElement) {
   ReactDOM.createRoot(rootElement).render(<RouterProvider router={router} />);
 }
->>>>>>> origin/main
